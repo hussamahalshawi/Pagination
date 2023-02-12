b18811b000bdc923b48d76837fcaa4c2571b36ba
hussamahalshawi <hussamahalshawi@gmail.com>
2023-02-12 16:54:52 +0200

    add pagination

--- terms/urls.py
+++ terms/urls.py
@@ -2,5 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AllKeywordsView.as_view(),name="home")
+    path("", views.AllKeywordsView.as_view(),name="home"),
+    path(
+        "terms",
+        views.KeywordListView.as_view(),
+        name="terms"
+    ),
+    path(
+        "terms/<int:page>",
+        views.listing,
+        name="terms-by-page"
+    ),
+    path(
+        "terms.json",
+        views.listing_api,
+        name="terms-api"
+    ),
 ]
